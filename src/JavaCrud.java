a541e6989d20b98828b5690b37235789c709d943
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2021-07-01 00:07:34 +0600

    Java Crud

---
+++ src/JavaCrud.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class JavaCrud {
+    private JPanel Main;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
